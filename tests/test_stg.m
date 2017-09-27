fcf4550f0776099010ff3c2d0165eefec515303c
gitlab@srinivas.gs <code@srinivas.gs>
2017-09-27 09:50:38 -0400

    added some alternative STG models

--- tests/test_stg.m
+++ tests/test_stg.m
@@ -7,7 +7,7 @@
 
 
 x = xolotl;
-x.addCompartment('AB',-70,0.05,10,0.0628,14.96,3000,0.05,200);
+x.addCompartment('AB',-65,0.02,10,0.0628,14.96,3000,0.05,200);
 
 x.addConductance('AB','prinz/NaV',1000,50);
 x.addConductance('AB','prinz/CaT',25,30);
@@ -17,7 +17,7 @@ x.addConductance('AB','prinz/KCa',50,-80);
 x.addConductance('AB','prinz/Kd',1000,-80);
 x.addConductance('AB','prinz/HCurrent',.1,-20);
 
-x.addCompartment('LP',-70,0.01,10,0.0628,14.96,3000,0.05,200);
+x.addCompartment('LP',-47,0.01,10,0.0628,14.96,3000,0.05,200);
 x.addConductance('LP','prinz/NaV',1000,50);
 x.addConductance('LP','prinz/CaS',40,30);
 x.addConductance('LP','prinz/ACurrent',200,-80);
@@ -25,7 +25,7 @@ x.addConductance('LP','prinz/Kd',250,-80);
 x.addConductance('LP','prinz/HCurrent',.5,-20);
 x.addConductance('LP','Leak',.3,-50);
 
-x.addCompartment('PY',-70,0.03,10,0.0628,14.96,3000,0.05,200);
+x.addCompartment('PY',-41,0.03,10,0.0628,14.96,3000,0.05,200);
 x.addConductance('PY','prinz/NaV',1000,50);
 x.addConductance('PY','prinz/CaT',25,30);
 x.addConductance('PY','prinz/CaS',20,30);
@@ -34,19 +34,47 @@ x.addConductance('PY','prinz/Kd',1250,-80);
 x.addConductance('PY','prinz/HCurrent',.5,-20);
 x.addConductance('PY','Leak',.1,-50);
 
+% 2a
 x.addSynapse('Chol','AB','LP',30);
-x.addSynapse('Chol','AB','PY',3);
-x.addSynapse('Glut','AB','LP',30);
-x.addSynapse('Glut','AB','PY',10);
+x.addSynapse('Chol','AB','PY',10);
+x.addSynapse('Glut','AB','LP',100);
+x.addSynapse('Glut','AB','PY',30);
 x.addSynapse('Glut','LP','PY',1);
-x.addSynapse('Glut','PY','LP',30);
+x.addSynapse('Glut','PY','LP',10);
 x.addSynapse('Glut','LP','AB',30);
+
+% fig 2c
+% x.addSynapse('Chol','AB','LP',100);
+% x.addSynapse('Chol','AB','PY',30);
+% x.addSynapse('Glut','AB','PY',1);
+% x.addSynapse('Glut','LP','PY',1);
+
+
+% fig 2d
+% x.addSynapse('Chol','AB','LP',100);
+% x.addSynapse('Chol','AB','PY',1);
+% x.addSynapse('Glut','AB','LP',3);
+% x.addSynapse('Glut','AB','PY',10);
+% x.addSynapse('Glut','LP','PY',1);
+% x.addSynapse('Glut','PY','LP',10);
+% x.addSynapse('Glut','LP','AB',10);
+
+
+% 2e
+% x.addSynapse('Chol','AB','LP',30);
+% x.addSynapse('Chol','AB','PY',3);
+% x.addSynapse('Glut','AB','LP',30);
+% x.addSynapse('Glut','AB','PY',10);
+% x.addSynapse('Glut','LP','PY',1);
+% x.addSynapse('Glut','PY','LP',30);
+% x.addSynapse('Glut','LP','AB',30);
+
 
 % x.transpile;
 % x.compile;
 
-x.dt = 50e-3;
-x.t_end = 5000;
+x.dt = 100e-3;
+x.t_end = 15000;
 
 [V,Ca] = x.integrate;
 
