611fa5be568425480d8c423956aff6fc51757e2e
Alec Hoyland <ahoyland@brandeis.edu>
2017-10-12 20:50:00 +0000

    Add external current

---
+++ addCurrent.m
@@ -0,0 +1,18 @@
+%              _       _   _
+%   __  _____ | | ___ | |_| |
+%   \ \/ / _ \| |/ _ \| __| |
+%    >  < (_) | | (_) | |_| |
+%   /_/\_\___/|_|\___/ \__|_|
+%
+% add external current to model
+
+function addCurrent(self,compartment,ext_current)
+	assert(nargin > 2,'No label! You have to label every current.')
+	% does the compartment exist?
+	assert(any(strcmp(compartment,properties(self))),'Unknown compartment')
+
+	assert(length(ext_current) == self.t_end/self.dt,'external current is the wrong length')
+
+	% add the current to the chosen compartment
+	self.(compartment).ext_current;
+end
