246ddb7c63a844562e25e1f5b6b6f29c2aa28c7c
srinivas.gs <github@srinivas.gs>
2018-01-20 19:03:24 -0500

    added discretized versions of Liu channels, which are 2.5X faster
    + discretized the m_inf, h_inf, tau_m and tau_h functions
    + with a resolution of 1 mV
    + this is then cached, and then the functions are never
    + evaluated again

---
+++ c++/conductances/liu-approx/KCa.hpp
@@ -0,0 +1,48 @@
+// _  _ ____ _    ____ ___ _    
+//  \/  |  | |    |  |  |  |    
+// _/\_ |__| |___ |__|  |  |___ 
+//
+// Slow Calcium conductance
+// http://www.jneurosci.org/content/jneuro/18/7/2309.full.pdf
+#ifndef KCA
+#define KCA
+#include "../../conductance.hpp"
+
+//inherit conductance class spec
+class KCa: public conductance {
+
+public:
+
+    // specify parameters + initial conditions 
+    KCa(double g_, double E_, double m_, double h_, double Q_g_, double Q_tau_m_, double Q_tau_h_)
+    {
+        gbar = g_;
+        E = E_;
+        m = m_;
+        h = 1;
+        
+
+        Q_g = Q_g_;
+        Q_tau_m = Q_tau_m_;
+        Q_tau_h = Q_tau_h_;
+    }
+    
+    void integrate(double V, double Ca, double dt, double delta_temp);
+    void connect(compartment *pcomp_);
+    double m_inf(double V, double Ca);
+    double tau_m(double V);
+};
+
+void KCa::connect(compartment *pcomp_) {container = pcomp_; }
+
+void KCa::integrate(double V, double Ca, double dt, double delta_temp)
+{
+    m = m_inf(V,Ca) + (m - m_inf(V,Ca))*exp(-dt/tau_m(V));
+    g = pow(Q_g, delta_temp)*gbar*m*m*m*m;
+}
+
+double KCa::m_inf(double V, double Ca) { return (Ca/(Ca+3.0))/(1.0+exp((V+28.3)/-12.6)); }
+double KCa::tau_m(double V) {return 90.3 - 75.1/(1.0+exp((V+46.0)/-22.7));}
+
+
+#endif
