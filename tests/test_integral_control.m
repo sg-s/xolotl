fbf1dd35f02edb99ad9e7a92a88a698beabf5d60
gitlab@srinivas.gs <code@srinivas.gs>
2018-03-19 17:37:17 -0400

    Prinz test now works

--- tests/test_integral_control.m
+++ tests/test_integral_control.m
@@ -1,11 +1,5 @@
 % tests a neuron that reproduces Fig 3 in Tim's paper
 
-try
-	x.cleanup;
-	clear x
-catch
-end
-
 vol = 1; % this can be anything, doesn't matter
 f = 1.496; % uM/nA
 tau_Ca = 200;
@@ -17,7 +11,7 @@ x = xolotl;
 x.cleanup;
 x.add('AB','compartment','Cm',10,'A',0.0628,'vol',vol,'phi',phi,'Ca_out',3000,'Ca_in',0.05,'tau_Ca',tau_Ca,'Ca_target',Ca_target);
 
-g0 = 1*rand(7,1);
+g0 = 10*rand(7,1);
 
 x.AB.add('liu/NaV','gbar',g0(1),'E',30);
 x.AB.add('liu/CaT','gbar',g0(2),'E',30);
@@ -26,7 +20,7 @@ x.AB.add('liu/ACurrent','gbar',g0(4),'E',-80);
 x.AB.add('liu/KCa','gbar',g0(5),'E',-80);
 x.AB.add('liu/Kd','gbar',g0(6),'E',-80);
 x.AB.add('liu/HCurrent','gbar',g0(7),'E',-20);
-x.AB.add('Leak','gbar',.99,'E',-50);
+x.AB.add('Leak','gbar',.099,'E',-50);
 
 tau_g = 5e3;
 
--- tests/test_prinz.m
+++ tests/test_prinz.m
@@ -3,45 +3,55 @@
 if ~exist('z','var')
 
 		
-	r0 = 0.0707; % mm
-	shell_thickness = 55e-3; % mm
-
-	vol = @(r) (4.*pi.*r.*r.*shell_thickness);
-	A = @(r) (4.*pi.*r.*r);
-
+	vol = 1; % this can be anything, doesn't matter
 	f = 14.96; % uM/nA
 	tau_Ca = 200;
 	F = 96485; % Faraday constant in SI units
-	phi = @(v) ((2*f*F*v)/tau_Ca);
-	Ca_target = 7; % used only when we add in homeostatic control 
-	
+	phi = (2*f*F*vol)/tau_Ca;
+
 
 	z = zoidberg;
 	G = z.findNeurons('burster');
+
+	x = xolotl;
+	x.cleanup;
+
+	x.add('AB','compartment','V',-65,'Ca',0.02,'Cm',10,'A',0.0628,'vol',vol,'phi',phi,'Ca_out',3000,'Ca_in',0.05,'tau_Ca',tau_Ca);
+
+	idx = randi(length(G));
+
+	x.AB.add('prinz/NaV','gbar',G(1,idx),'E',50);
+	x.AB.add('prinz/CaT','gbar',G(2,idx),'E',30);
+	x.AB.add('prinz/CaS','gbar',G(3,idx),'E',30);
+	x.AB.add('prinz/ACurrent','gbar',G(4,idx),'E',-80);
+	x.AB.add('prinz/KCa','gbar',G(5,idx),'E',-80);
+	x.AB.add('prinz/Kd','gbar',G(6,idx),'E',-80);
+	x.AB.add('prinz/HCurrent','gbar',G(7,idx),'E',-20);
+	x.AB.add('Leak','gbar',G(8,idx),'E',-50);
+
+
 end
 
 idx = randi(length(G));
 
-x = xolotl;
-x.addCompartment('AB',-60,0.02,10,A(r0),vol(r0),phi(vol(r0)),3000,0.05,tau_Ca,0);
-
-
-x.addConductance('AB','prinz/NaV',G(1,idx),50);
-x.addConductance('AB','prinz/CaT',G(2,idx),30);
-x.addConductance('AB','prinz/CaS',G(3,idx),30);
-x.addConductance('AB','prinz/ACurrent',G(4,idx),-80);
-x.addConductance('AB','prinz/KCa',G(5,idx),-80);
-x.addConductance('AB','prinz/Kd',G(6,idx),-80);
-x.addConductance('AB','prinz/HCurrent',G(7,idx),-20);
-x.addConductance('AB','Leak',G(8,idx),-50);
+x.set('AB.NaV.gbar',G(1,idx))
+x.set('AB.CaT.gbar',G(2,idx))
+x.set('AB.CaS.gbar',G(3,idx))
+x.set('AB.ACurrent.gbar',G(4,idx))
+x.set('AB.KCa.gbar',G(5,idx))
+x.set('AB.Kd.gbar',G(6,idx))
+x.set('AB.HCurrent.gbar',G(7,idx))
+x.set('AB.Leak.gbar',G(8,idx))
 
 [V,Ca] = x.integrate;
 
 if usejava('jvm')
+	close all
 	figure, hold on
 	subplot(2,1,1); hold on
 	plot(V)
 	ylabel('Voltage (mV)')
+	title(mat2str(G(:,idx)))
 	subplot(2,1,2)
 	plot(Ca(:,1));
 	ylabel('[Ca] (\muM)')
