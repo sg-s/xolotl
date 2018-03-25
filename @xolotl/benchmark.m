a7006693f00fc2f5846e4af08e6d84f5e6d71619
srinivas.gs <github@srinivas.gs>
2018-03-25 11:57:49 -0400

    improvements to benchmark.m

--- @xolotl/benchmark.m
+++ @xolotl/benchmark.m
@@ -7,7 +7,7 @@
 % benchmarks a model with varying dt
 % 
 
-function best_dt = benchmark(self)
+function benchmark(self)
 
 original_dt = self.sim_dt;
 original_t_end = self.t_end;
@@ -16,23 +16,22 @@ original_state = self.closed_loop;
 self.t_end = 5e3;
 self.closed_loop = false;
 
-dt = 1000; 
-K = 1:dt;
-all_dt = K(rem(dt,K) == 0);
+max_dt = 1000; 
+K = 1:max_dt;
+all_dt = K(rem(max_dt,K) == 0);
 all_dt = all_dt/1e3;
 
-all_r2 = NaN*all_dt;
+all_Q = NaN*all_dt;
 all_speed = NaN*all_dt;
 
 self.skip_hash_check = true;
 
-
 self.sim_dt = all_dt(1);
-self.dt = 1;
+self.dt = max_dt*1e-3;
 tic
 V0 = self.integrate;
+V0_diff = diff(V0);
 all_speed(1) = toc;
-
 
 
 for i = 2:length(all_dt)
@@ -42,36 +41,60 @@ for i = 2:length(all_dt)
 	V = self.integrate;
 	all_speed(i) = toc;
 
+	% measure distance b/w diff-embedded attractors
+	V_diff = diff(V);
 
-	this_r2 = NaN(size(V,2),1);
-	for j = 1:size(V,2)
-		this_r2(j) = rsquare(V(:,j),V0(:,j));
+	this_cost = 0;
+	for j = 2:10:length(V0_diff)
+		for k = 1:size(V,2)
+			this_cost = this_cost + min(sqrt((V0_diff(j,k) - V_diff(:,k)).^2 + (V0(2:end,k) - V(2:end,k)).^2));
+		end
 	end
 
-	all_r2(i) = mean(this_r2);
+	all_r2(i) = this_cost;
 
 end
 
 
-if ~nargout
-	figure('outerposition',[0 0 500 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
-	yyaxis left
-	plot(all_dt,all_r2,'k-+')
-	set(gca,'YScale','log')
-	xlabel('dt (ms)')
-	ylabel('Quality (r^2)')
-	set(gca,'YLim',[0 1])
+figure('outerposition',[0 0 1001 500],'PaperUnits','points','PaperSize',[1000 500]); hold on
+subplot(1,2,1); hold on
+yyaxis left
+plot(all_dt,all_r2,'-+')
+set(gca,'YScale','log')
+xlabel('dt (ms)')
+ylabel('Error (a.u.)')
 
-	yyaxis right
-	plot(all_dt,(self.t_end/1e3)./all_speed,'-o')
-	ylabel('Speed (X realtime)')
-	set(gca,'YScale','log','XScale','log')
+yyaxis right
+plot(all_dt,(self.t_end/1e3)./all_speed,'-o')
+ylabel('Speed (X realtime)')
+set(gca,'YScale','log','XScale','log')
 
-	prettyFig();
 
-else
-	best_dt = all_dt(find(all_r2 < .99,1,'first'))/2;
+% now vary t_end to see the overhead costs 
+self.sim_dt = .1;
+self.closed_loop = true;
+all_t_end = round(logspace(1,6,20));
+
+all_T = NaN*length(all_t_end);
+
+for i = 1:length(all_t_end)
+	self.t_end = all_t_end(i);
+	tic;
+	self.integrate;
+	all_T(i) = toc;
 end
+
+all_T = all_T*1e3;
+
+subplot(1,2,2); hold on
+plot(all_t_end,all_t_end./all_T,'-o')
+xlabel('Simulation time (ms)')
+set(gca,'XScale','log','YScale','log','XTick',logspace(1,6,6))
+ylabel('Speed (X realtime)')
+
+prettyFig();
+
+
 
 self.sim_dt = original_dt;
 self.t_end = original_t_end;
