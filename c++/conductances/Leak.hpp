dc9de68c77890e6e95adda93b1121c5fda34dce8
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-08 17:08:05 -0400

    Run test

--- c++/conductances/Leak.hpp
+++ c++/conductances/Leak.hpp
@@ -1,6 +1,6 @@
-// _  _ ____ _    ____ ___ _    
-//  \/  |  | |    |  |  |  |    
-// _/\_ |__| |___ |__|  |  |___ 
+// _  _ ____ _    ____ ___ _
+//  \/  |  | |    |  |  |  |
+// _/\_ |__| |___ |__|  |  |___
 //
 // Slow Calcium conductance
 #ifndef LEAK
@@ -12,22 +12,23 @@ class Leak: public conductance {
 
 public:
 
-    // specify parameters + initial conditions 
+    // specify parameters + initial conditions
     Leak(double g_, double E_)
     {
         gbar = g_;
-        g = gbar; // this is important as integrate doesn't do anything in the leak channels 
+        g = gbar; // this is important as integrate doesn't do anything in the leak channels
         E = E_;
 
         if (isnan (E)) { E = -55; }
 
     }
-    
+
 
 
     void integrate(double V, double Ca, double dt, double delta_temp);
     void connect(compartment *pcomp_);
     string getClass(void);
+    double getCurrent(double V, double Ca);
 };
 
 string Leak::getClass(){return "Leak";}
@@ -37,5 +38,8 @@ void Leak::connect(compartment *pcomp_) {container = pcomp_;}
 void Leak::integrate(double V, double Ca, double dt, double delta_temp) {
     // do nothing
 }
+
+double Leak::getCurrent(double V, double Ca) {return gbar*(V-E);}
+
 
 #endif
