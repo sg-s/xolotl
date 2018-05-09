6402ab49aa55416ca478fbc7eb1087e0b8d57c44
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-09 12:45:25 -0400

    Add getCurrent() method

--- c++/conductances/hara/Cac.hpp
+++ c++/conductances/hara/Cac.hpp
@@ -1,6 +1,6 @@
-// _  _ ____ _    ____ ___ _    
-//  \/  |  | |    |  |  |  |    
-// _/\_ |__| |___ |__|  |  |___ 
+// _  _ ____ _    ____ ___ _
+//  \/  |  | |    |  |  |  |
+// _/\_ |__| |___ |__|  |  |___
 //
 // Cac channels in Drosophila muscles
 // https://www.ncbi.nlm.nih.gov/pubmed/26004544
@@ -13,7 +13,7 @@ class Cac: public conductance {
 
 public:
 
-    // specify parameters + initial conditions 
+    // specify parameters + initial conditions
     Cac(double g_, double E_, double m_, double h_)
 {
     gbar = g_;
@@ -33,8 +33,10 @@ public:
     double m_inf(double V);
     double h_inf(double V);
     double tau_m(double V);
-    double tau_h(double V); 
+    double tau_h(double V);
     string getClass(void);
+    double getCurrent(double V, double Ca);
+
 };
 
 string Cac::getClass(){return "Cac";}
@@ -43,13 +45,13 @@ void Cac::connect(compartment *pcomp_) {container = pcomp_; }
 
 void Cac::integrate(double V, double Ca, double dt, double delta_temp)
 {
-    // update E by copying E_Ca from the cell 
+    // update E by copying E_Ca from the cell
     E = container->E_Ca;
     m = m_inf(V) + (m - m_inf(V))*exp(-dt/tau_m(V));
     h = h_inf(V) + (h - h_inf(V))*exp(-dt/tau_h(V));
     g = gbar*m*h;
 
-    // compute the specific calcium current and update it in the cell 
+    // compute the specific calcium current and update it in the cell
     double this_I = g*(V-E);
     container->i_Ca += this_I;
 
@@ -59,5 +61,8 @@ double Cac::m_inf(double V) {return 1.0/(1.0+exp((V-3.68)/-6.35));}
 double Cac::h_inf(double V) {return 1.0/(1.0+exp((V+9.17)/8.22));}
 double Cac::tau_m(double V) {return 2.8 + 14.0/(exp((V+27.0)/10.0) + exp((V+70.0)/-13.0));}
 double Cac::tau_h(double V) {return 1400;}
+
+double Cac::getCurrent(double V, double Ca) {return gbar*m*m*m*(V-E);}
+
 
 #endif
