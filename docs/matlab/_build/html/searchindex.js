78b293697291c2308b59143ac04adeaa460adfb3
Alec Hoyland <alec.hoyland@gmail.com>
2018-05-09 22:10:27 -0400

    Begin using sphinx

---
+++ docs/matlab/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["xolotl_documentation"],envversion:53,filenames:["xolotl_documentation.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to xolotl\u2019s documentation!"],titleterms:{document:0,indic:0,tabl:0,welcom:0,xolotl:0}})
