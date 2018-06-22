c60885f28c1eb1a4dd0b9af39e6d5a70e64cdddc
Alec Hoyland <ahoyland@brandeis.edu>
2018-06-21 20:07:59 -0400

    Fix error with conductances

--- tests/test_conductances.m
+++ tests/test_conductances.m
@@ -24,9 +24,9 @@ phi = (2*f*F*vol)/tau_Ca;
 x.add('AB','compartment','Cm',10,'A',0.0628,'vol',vol,'phi',phi,'Ca_out',3000,'Ca_in',0.05,'tau_Ca',tau_Ca);
 
 % add dethier conductances
-% x.AB.add('dethier/CaT', 'gbar', 1);
-% x.AB.add('dethier/HCurrent', 'gbar', 1, 'E', -20);
-% x.AB.add('dethier/Kd', 'gbar', 1, 'E', -80);
+x.AB.add('dethier/CaT', 'gbar', 1);
+x.AB.add('dethier/HCurrent', 'gbar', 1, 'E', -20);
+x.AB.add('dethier/Kd', 'gbar', 1, 'E', -80);
 x.AB.add('dethier/NaV', 'gbar', 1, 'E', -50);
 
 plot(x.integrate);
