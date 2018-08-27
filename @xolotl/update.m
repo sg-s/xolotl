3e1c62c2c51d8207299104546e85a91e95e6d50d
gitlab@srinivas.gs <code@srinivas.gs>
2018-08-27 16:15:07 -0400

    better windows updating

--- @xolotl/update.m
+++ @xolotl/update.m
@@ -41,7 +41,12 @@ else
 	if any(strcmp({toolboxes.Name},'xolotl'))
 		% xolotl installed as a toolbox
 		% go somewhere safe
-		cd(matlabroot)
+		if ispc
+			gohere = winqueryreg('HKEY_CURRENT_USER',['Software\Microsoft\Windows\CurrentVersion\','Explorer\Shell Folders'],'Personal');
+		else
+			gohere = '~';
+		end
+		cd(gohere)
 
 		% remove all toolboxes with "xolotl" in it
 		for i = 1:length(toolboxes)
