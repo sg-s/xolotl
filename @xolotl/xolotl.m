bb3048c08c4f7cb01ddb0ea877171cae5b4c0724
Alec Hoyland <ahoyland@brandeis.edu>
2018-11-28 19:37:55 -0500

    Force list of static methods to be the same as in master

--- @xolotl/xolotl.m
+++ @xolotl/xolotl.m
@@ -248,9 +248,13 @@ methods (Static)
 
     [passed, total] = run_all_tests(cleanup);
 
+    C = matrixCost(M1,M2);
+    [M, V_lim, dV_lim] = V2matrix(V, V_lim, dV_lim);
+
     cleanup;
     curr_index = contributingCurrents(V,I);
-    C = coincidence(model_spiketimes, data_spiketimes, dt, Delta);
+    spiketimes = findNSpikeTimes(V,n_spikes,on_off_thresh);
+    f = findNSpikes(V, on_off_thresh);
     ax = show(conductance,ax);
     [m_inf, h_inf, tau_m, tau_h] =  getGatingFunctions(conductance);
 
