dafccfc40aa04f97321acfaa3c053c6e4fa96f1d
Alec Hoyland <ahoyland@brandeis.edu>
2019-01-12 19:09:23 -0500

    Update documentation

--- @xtools/matrixCost.m
+++ @xtools/matrixCost.m
@@ -1,27 +1,29 @@
 %{
-           _       _   _
-__  _____ | | ___ | |_| |
-\ \/ / _ \| |/ _ \| __| |
->  < (_) | | (_) | |_| |
-/_/\_\___/|_|\___/ \__|_|
+              _       _   _
+   __  _____ | | ___ | |_| |
+   \ \/ / _ \| |/ _ \| __| |
+    >  < (_) | | (_) | |_| |
+   /_/\_\___/|_|\___/ \__|_|
 
-matrixCost
-^^^^^^^^^^
-a static method to compute the distance between two LeMasson matrices. This is a useful way to determine how similar two voltage traces are.
+### matrixCost
 
-Usage ::
+**Syntax**
 
-	C = matrixCost(M1,M2)
+```matlab
+	C = xtools.matrixCost(M1, M2)
+```
 
-where ``M1`` and ``M2`` are two matrices returned by xtools.V2matrix() and represent discretized probability distributions of a derivative-embedded attractor of the voltage trace.
+**Description**
 
+Compute the norm-squared distance between two matrices. If the matrices are LeMasson
+matrices, which represent discretized probability distributions of a derivative-embedded
+attractor of a voltage trace, then this distance serves as a measure of how dissimilar
+the two voltage traces are.
 
-See Also
---------
+!!! info "See Also"
+    ->xtools.V2Matrix
+		LeMasson G, Maex R (2001) Introduction to equation solving and parameter fitting. In: De Schutter E (ed) Computational Neuroscience: Realistic Modeling for Experimentalists. CRC Press, London pp 1–21
 
-LeMasson G, Maex R (2001) Introduction to equation solving and parameter fitting. In: De Schutter E (ed) Computational Neuroscience: Realistic Modeling for Experimentalists. CRC Press, London pp 1–21
-
-- xtools.V2matrix
 
 %}
 
