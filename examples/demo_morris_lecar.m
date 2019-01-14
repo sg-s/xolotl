c772e0cbb7fa891d5d0723888c08656b7cf7cd56
Alec Hoyland <ahoyland@brandeis.edu>
2019-01-14 15:55:38 -0500

    Set default injected current to 3 nA

--- examples/demo_morris_lecar.m
+++ examples/demo_morris_lecar.m
@@ -1,7 +1,9 @@
 x = xolotl;
 
-x.add('compartment', 'ML', 'Cm', 1, 'A', 0.01);
+x.add('compartment', 'ML', 'Cm', 10, 'A', 0.01);
 x.ML.add('MorrisLecar');
+
+x.I_ext = 3;
 
 x.pref.show_Ca = false;
 x.pref.plot_color = false;
