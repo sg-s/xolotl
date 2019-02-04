f0693c54527ea113fdb5e6b12ce1096ad7e1acc6
gitlab@srinivas.gs <code@srinivas.gs>
2019-02-04 15:12:21 -0500

    wrote manual_install

---
+++ manual_install.m
@@ -0,0 +1,10 @@
+% manually installs xolotl
+% does not use MATLAB's toolbox because
+% versions installed that way are slower (really)
+% UX elements are slower, which means
+% manipulate is too slow to use
+
+addpath(pwd)
+addpath('../cpplab')
+addpath('../puppeteer')
+addpath(genpath('../srinivas.gs_mtools/src'))
