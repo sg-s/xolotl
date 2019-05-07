15bb8447b31e4040e55d0299fea6125d09e2bffc
gitlab@srinivas.gs <code@srinivas.gs>
2019-05-07 15:46:16 -0400

    added two-compartment STG-like model in the examples

---
+++ +xolotl/+examples/TwoCompartmentSTG.m
@@ -0,0 +1,57 @@
+% makes a two-compartment neuron model
+% that is meant to be fit to the dynamics of a STG neuron
+% 
+% In this model, the CellBody has all the normal STG channels
+% except NaV
+% and the axon only has NaV, Kd and ACurrent
+function x = TwoCompartmentSTG(resistivity)
+
+if nargin == 0
+	resistivity = 1e-3; % MOhm mm; 
+end
+
+
+r_soma = .025;
+L_soma = .025; % mm, Jason's guesstimates
+f = 14.96;
+
+r_neurite = .01;
+L_neurite = .35; % mm, from Otopalik et al
+
+shell_thickness = .01; % 10 micrometres
+
+x = xolotl;
+
+x.add('compartment','CellBody','radius',r_soma,'len',L_soma,'Ca_out',3000,'tree_idx',0,'shell_thickness',shell_thickness);
+x.CellBody.add('prinz/Calcium','f',f)
+
+
+x.add('compartment','Axon','radius',r_neurite,'len',L_neurite,'Ca_out',3000);
+x.Axon.add('prinz/Calcium','f',f)
+
+
+
+% soma has everythign except NaV
+x.CellBody.add('prinz/CaT','gbar',25);
+x.CellBody.add('prinz/CaS','gbar',60);
+x.CellBody.add('prinz/ACurrent','gbar',500);
+x.CellBody.add('prinz/KCa','gbar',50);
+x.CellBody.add('prinz/Kd','gbar',1e3);
+x.CellBody.add('prinz/HCurrent','gbar',.1);
+x.CellBody.add('Leak','gbar',.1)
+
+% axon has NaV, Kd, Leak, ACurrent
+x.Axon.add('prinz/NaV','gbar',1000);
+x.Axon.add('prinz/Kd','gbar',1000);
+x.Axon.add('prinz/ACurrent','gbar',500);
+x.Axon.add('Leak','gbar',.1)
+
+x.CellBody.tree_idx = 0;
+x.connect('CellBody','Axon','Axial','resistivity',resistivity);
+x.connect('Axon','CellBody','Axial','resistivity',resistivity);
+
+
+
+x.dt = .1;
+x.sim_dt = .05;
+x.t_end = 5e3;
