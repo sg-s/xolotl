47f7ee7e36d2bcf8eb7c88769be919da19a2f143
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-01 13:26:59 -0400

    Add CaN current

---
+++ c++/conductances/destexhe/CaN.hpp
@@ -0,0 +1,60 @@
+// _  _ ____ _    ____ ___ _
+//  \/  |  | |    |  |  |  |
+// _/\_ |__| |___ |__|  |  |___
+//
+// C++ dependent nonspecific cation current
+// Destexhe, Contreras, Sejnowski, & Steriade 1994
+// https://www.ncbi.nlm.nih.gov/pubmed/7527077
+
+#ifndef CAN
+#define CAN
+#include "conductance.hpp"
+
+//inherit conductance class spec
+class CaN: public conductance {
+
+public:
+
+    // tuning parameters for the kinetics
+    double alpha;
+    double beta;
+
+    // specify parameters + initial conditions
+    CaN(double g_, double E_, double m_, double alpha_, double beta_)
+    {
+        gbar = g_;
+        E = E_;
+        m = m_;
+
+        // tuning parameters for the kinetics
+        alpha = alpha_;
+        beta = beta_;
+
+        // defaults
+        if (isnan(gbar)) { gbar = 0; }
+        if (isnan (E)) { E = -20; }
+        if (isnan (alpha)) { alpha = 20; }
+        if (isnan (beta)) { beta = 0.002; }
+
+        p = 2;
+
+        // allow approximations
+        approx_m = 1;
+
+    }
+
+    double m_inf(double, double);
+    double tau_m(double, double);
+    string getClass(void);
+
+};
+
+string CaN::getClass(){
+    return "CaN";
+}
+
+double CaN::m_inf(double V, double Ca) {return alpha * fast_pow(Ca, 2) / (alpha * fast_pow(Ca, 2) + beta);}
+double CaN::tau_m(double V, double Ca) {return 1 / (alpha * fast_pow(Ca, 2) + beta);}
+
+
+#endif
