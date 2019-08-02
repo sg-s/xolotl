a11802f1d410345cfacdde9b1198fc21438acb70
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-02 15:44:38 -0400

    Add axis label

--- examples/demo_finite_size.m
+++ examples/demo_finite_size.m
@@ -3,7 +3,7 @@
 % In this demo, we switch from a deterministic model
 % of channel and subunit kinetics to a stochastic model
 % that we integrate using the Euler-Maruyama method
-% using the approximate Langevin method 
+% using the approximate Langevin method
 
 
 x = xolotl();
@@ -56,7 +56,7 @@ for i = 6:-1:1
 	set(gca,'YLim',[-90 50],'XLim',[0 1])
 	th = title([strlib.oval(x.AB.A*1e6) 'um^2']);
 	th.FontWeight = 'normal';
-	
+
 	if i < 6
 		axis off
 	end
@@ -86,6 +86,7 @@ isis = diff(st);
 axisi = subplot(2,2,4); hold on
 [hy,hx] = histcounts(isis);
 stairs(axisi,hx(2:end)+mean(diff(hx)),hy,'k')
+xlabel('Interspike interval (ms)')
 
 figlib.pretty()
 
