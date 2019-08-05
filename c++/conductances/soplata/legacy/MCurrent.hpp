4eb49db91ad81256cebffff0ef89c797e223ad4f
Alec Hoyland <alec.hoyland@posteo.net>
2019-08-05 15:32:20 -0400

    Update Soplata conductances (#455)

--- c++/conductances/soplata/legacy/MCurrent.hpp
+++ c++/conductances/soplata/legacy/MCurrent.hpp
@@ -2,22 +2,22 @@
 //  \/  |  | |    |  |  |  |
 // _/\_ |__| |___ |__|  |  |___
 //
-// delayed rectifier potassium conductance
+// slow potassium current
 // Soplata AE, McCarthy MM, Sherfey J, Lee S, Purdon PL, Brown EN, et al.
 // Thalamocortical control of propofol phase-amplitude coupling. PLOS
 // Computational Biology. 2017;13: e1005879. doi:10.1371/journal.pcbi.1005879
 
-#ifndef KD
-#define KD
+#ifndef MCURRENT
+#define MCURRENT
 #include "conductance.hpp"
 
 //inherit conductance class spec
-class Kd: public conductance {
+class MCurrent: public conductance {
 
 public:
 
     //specify both gbar and erev and initial conditions
-    Kd(double g_, double E_, double m_)
+    MCurrent(double g_, double E_, double m_)
     {
         gbar = g_;
         E = E_;
@@ -44,13 +44,13 @@ public:
 
 };
 
-string Kd::getClass(){
-    return "Kd";
+string MCurrent::getClass(){
+    return "MCurrent";
 }
 
-double Kd::a_m(double V, double Ca) {return 0.032*(15.0-(V+25.0))/(exp((15.0-(V+25.0))/5.0)-1.0); }
-double Kd::b_m(double V, double Ca) {return 0.5*exp((10-(V+25.0))/40.0); }
-double Kd::m_inf(double V, double Ca) {return a_m(V,Ca) / (a_m(V,Ca)+b_m(V,Ca)); }
-double Kd::tau_m(double V, double Ca) {return 1.0 / (a_m(V,Ca) + b_m(V,Ca)); }
+double MCurrent::a_m(double V, double Ca) {return 0.0001 * 3.209 * (V + 30) / (1 - exp(-(V+30)/9)); }
+double MCurrent::b_m(double V, double Ca) {return -0.0001 * 3.209 * (V + 30) / (1 - exp((V+30)/9)); }
+double MCurrent::m_inf(double V, double Ca) {return a_m(V,Ca) / (a_m(V,Ca)+b_m(V,Ca)); }
+double MCurrent::tau_m(double V, double Ca) {return 1.0 / (a_m(V,Ca) + b_m(V,Ca)); }
 
 #endif
