5a3a2b97190a0b8e0b19358163aad43ba37783f4
Alec Hoyland <alec.hoyland@posteo.net>
2019-12-06 13:41:19 -0500

    Docs fixes 2 (#483)
    * Windows works with GitHub Desktop
    
    * Add adaptive sampling demo
    
    * Add adaptive sampling demo
    
    * Add comments and colorbar

--- examples/demo_adaptive_sampling.m
+++ examples/demo_adaptive_sampling.m
@@ -1,21 +1,30 @@
 % In this demo, we show how we can use adaptive sampling
 % to efficiently map parameter space
 
+%% Create a single-compartment neuron model
 x = xolotl.examples.BurstingNeuron('prefix','liu');
 x.dt = .1;
 x.sim_dt = .1;
 x.t_end = 10e3;
 
+% choose which parameters to vary
 parameters_to_vary = {'*.HCurrent.gbar','*.CaS.gbar'};
 
+%% Create the adaptive sampler object
 a = adaptive('MaxIter',20);
+
+% create the sampling function
+% here, we are sampling the burst period
+% and varying the H current and CaS maximal conductances
 a.SampleFcn = @(params) xtools.sampleMetrics(x,[],parameters_to_vary,params);
 
+% set up lower and upper bounds for params
 a.Lower = [0 0];
 a.Upper = [20 100];
 
 a.UseParallel = true;
 
+%% Generate a figure
 figure('outerposition',[300 300 601 600],'PaperUnits','points','PaperSize',[601 600]); hold on
 a.PlotHere = gca;
 
@@ -23,5 +32,8 @@ figlib.pretty('LineWidth',1)
 
 xlabel('g_H (uS/mm^2)')
 ylabel('g_{CaS} (uS/mm^2)')
+c = colorbar;
+c.Label.String = 'burst period (ms)';
 
+%% Perform the sampling and plot onto the created figure
 a.sample
