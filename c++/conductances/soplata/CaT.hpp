39fa25ac722d41e9b5baf541919eb7259fa785b6
Alec Hoyland <alec.hoyland@posteo.net>
2020-03-26 12:36:09 -0400

    Update conductance

--- c++/conductances/soplata/CaT.hpp
+++ c++/conductances/soplata/CaT.hpp
@@ -2,9 +2,25 @@
 //  \/  |  | |    |  |  |  |
 // _/\_ |__| |___ |__|  |  |___
 //
-// component info: Transient Ca current
-// component source [Liu et al. 98](http://www.jneurosci.org/content/jneuro/18/7/2309.full.pdf)
+// component info: T-type calcium current
+// component source: [Soplata et al. 2017](https://www.ncbi.nlm.nih.gov/pubmed/29227992)
 //
+// direct link to supplementary info: https://journals.plos.org/ploscompbiol/article/file?type=supplementary&id=info:doi/10.1371/journal.pcbi.1005879.s001
+// direct link to GitHub repository: https://github.com/asoplata/propofol-coupling-2017-full
+//
+// Soplata AE, McCarthy MM, Sherfey J, Lee S, Purdon PL, Brown EN, et al.
+// (2017) Thalamocortical control of propofol phase-amplitude coupling. PLoS
+// Comput Biol 13(12): e1005879. https://doi.org/10.1371/journal.pcbi.1005879
+//
+// dynamics were inherited from Ching et al. 2010,
+// but diverge from the original publication
+//
+// Ching, S., Cimenser, A., Purdon, P. L., Brown, E. N., & Kopell, N. J.
+// (2010). Thalamocortical model for a propofol-induced alpha-rhythm
+// associated with loss of consciousness. Proceedings of the National
+// Academy of Sciences, 107(52), 22665–22670.
+// http://doi.org/10.1073/pnas.1017069108
+
 #ifndef CAT
 #define CAT
 #include "conductance.hpp"
@@ -26,7 +42,7 @@ public:
         if (isnan(gbar)) { gbar = 0; }
         if (isnan (E)) { E = 30; }
 
-        p = 3;
+        p = 2;
         q = 1;
 
         // allow this channel to be approximated
@@ -37,7 +53,7 @@ public:
 
     }
 
-
+    void integrate(double, double);
     double m_inf(double, double);
     double h_inf(double, double);
     double tau_m(double, double);
@@ -52,10 +68,63 @@ string CaT::getClass(){
 }
 
 
-double CaT::m_inf(double V, double Ca) {return 1.0/(1.0 + exp((V+27.1)/-7.2));}
-double CaT::h_inf(double V, double Ca) {return 1.0/(1.0 + exp((V+32.1)/5.5));}
-double CaT::tau_m(double V, double Ca) {return 21.7 - 21.3/(1.0 + exp((V+68.1)/-20.5));}
-double CaT::tau_h(double V, double Ca) {return 105.0 - 89.8/(1.0 + exp((V+55.0)/-16.9));}
+double CaT::m_inf(double V, double Ca) {return 1 / (1 + exp((-(V + 2) + 57)/6.2));}
+double CaT::h_inf(double V, double Ca) {return 1 / (1 + exp(((V + 2) + 81)/4));}
+double CaT::tau_m(double V, double Ca) {return 0;}
+double CaT::tau_h(double V, double Ca) {return (30.8 + (211.4 * exp(((V + 2) + 113.2)/5))/(1 + exp(((V + 2) + 84)/3.2))) / 3.73;}
+
+// define a custom integrate method
+// to deal with the fact that the m-kinetics are instantaneous
+
+void CaT::integrate(double V, double Ca) {
+
+
+    if (is_calcium) {
+        E = container->E_Ca;
+    }
+
+    V_idx = (int) round((V*10)+999);
+    if (V_idx < 0) {V_idx = 0;};
+    if (V_idx > 2000) {V_idx = 2000;};
+
+    // assume that p > 0
+    switch (approx_m) {
+        case 0:
+            minf = m_inf(V,Ca);
+            m = minf;
+            break;
+
+        default:
+            m = m_inf_cache[V_idx];
+    } // switch approx_m
+
+    g = gbar*fast_pow(m,p);
+
+    switch (q) {
+        case 0:
+            break;
+        default:
+            switch (approx_h) {
+                case 0:
+                    hinf = h_inf(V,Ca);
+                    h = hinf + (h - hinf)*exp(-dt/tau_h(V,Ca));
+                    break;
+                default:
+                    h = h_inf_cache[V_idx] + (h - h_inf_cache[V_idx])*fast_exp(-(dt/tau_h_cache[V_idx]));
+                    break;
+            }
+
+            g = g*fast_pow(h,q);
+            break;
+    } // switch q
+
+
+    if (is_calcium) {
+        container->i_Ca += getCurrent(V);
+    }
+
+}
+
 
 
 #endif
