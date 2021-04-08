aec6586625a99e748c09c84dc24972439521b4b8
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-04-08 12:19:23 -0400

    Added H-current (Hill et al., 2001)

---
+++ c++/conductances/hill/HCurrent.hpp
@@ -0,0 +1,44 @@
+// _  _ ____ _    ____ ___ _
+//  \/  |  | |    |  |  |  |
+// _/\_ |__| |___ |__|  |  |___
+//
+// Hyperpolarization activated inward  (H) current in the model of leech heartbeat HCO neurons (Hill et al., 2001)
+
+#ifndef HCURRENT
+#define HCURRENT
+
+//inherit conductance class spec
+class HCurrent: public conductance {
+
+public:
+
+    //specify both gbar and erev and initial conditions
+    HCurrent(double gbar_, double E_, double m_)
+    {
+        gbar = gbar_;
+        E = E_;
+        m = m_;
+
+        // defaults 
+        if (isnan(gbar)) { gbar = 0; }
+        
+        if (isnan (E)) { E = -21; }
+		
+
+    // exponents of m
+        p = 2;
+
+	name = "HCurrent";			
+    }
+	
+    double m_inf(double, double);
+    double tau_m(double, double);
+	
+
+};
+
+
+double HCurrent::m_inf(double V, double Ca) {return 1.0/(1.0 + 2.0*exp((V+47.0)*0.18) + exp((V+47.0)*0.5));}
+double HCurrent::tau_m(double V, double Ca) {return 700.0 + 1700.0/(1.0+exp((V+73.0)*-0.1));}
+
+#endif
