f96360ca524980b90918778208dc6125c77de673
Ekaterina Morozova <morozova.e.o@gmail.com>
2021-04-08 12:23:42 -0400

    Added persistent Na current (Hill et al., 2001)

---
+++ c++/conductances/hill/NaP.hpp
@@ -0,0 +1,40 @@
+// _  _ ____ _    ____ ___ _
+//  \/  |  | |    |  |  |  |
+// _/\_ |__| |___ |__|  |  |___
+//
+// Persistent Na current in the model of leech heartbeat HCO neurons (Hill et al., 2001)
+
+#ifndef NAP
+#define NAP
+
+//inherit conductance class spec
+class NaP: public conductance {
+
+public:
+
+    //specify both gbar and erev and initial conditions
+    NaP(double gbar_, double E_, double m_)
+    {
+        gbar = gbar_;
+        E = E_;
+        m = m_;
+
+        // defaults 
+        if (isnan(gbar)) { gbar = 0; }
+        
+        if (isnan (E)) { E = 45; }
+		
+	name = "NaP";
+    }
+	
+    double m_inf(double, double);
+    double tau_m(double, double);	
+	
+};
+
+
+double NaP::m_inf(double V, double Ca) {return 1.0/(1.0+exp((V+39.0)*-0.12));}
+double NaP::tau_m(double V, double Ca) {return 10.0 + 200.0/(1.0+exp(0.4*(V+57.0)));}
+
+
+#endif
